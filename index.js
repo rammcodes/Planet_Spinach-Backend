51f6cfff81e47fe445323c958888a9f772cd7c36
Ram <ramdevukd@gmail.com>
2019-11-06 18:43:42 +0530

    configured port

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //-----------------
+app.get("/", (req, res) => res.send("Spinaccch"));
 app.use("/user", user);
 app.use("/product", product);
 app.use("/basket", basket);
 app.use("/admin", admin);
 
 //-----------------
-app.listen(5000, () => {
-  console.log("App listening on PORT 5000 ");
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`App listening on PORT ${process.env.PORT || 5000}`);
 });
